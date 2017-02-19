e891fe954f8692cdbb70eabe42a7d25f81a54ccb
shalikram <sapkotashalikram@gmail.com>
2017-02-20 00:00:30 +0200

    Add files via upload

---
+++ payment.js
@@ -0,0 +1,58 @@
+{
+   "paymentMethods":[
+      {
+         "brandCode":"ideal",
+         "name":"iDEAL",
+       "logo":"https://live.adyen.com/hpp/img/pm/ideal_small.png",
+         "issuers":[
+            {
+               "issuerId":"1121",
+               "name":"Test Issuer"
+            },
+            {
+               "issuerId":"1152",
+               "name":"Test Issuer 3"
+            },
+            {
+               "issuerId":"1151",
+               "name":"Test Issuer 2"
+            }
+         ]
+      },
+      {
+         "brandCode":"sepadirectdebit",
+         "name":"SEPA Direct Debit",
+       "logo":"https://live.adyen.com/hpp/img/pm/sepadirectdebit_small.png"
+      },
+      {
+         "brandCode":"moneybookers",
+         "name":"Moneybookers",
+       "logo":"https://live.adyen.com/hpp/img/pm/moneybookers_small.png"
+      },
+      {
+         "brandCode":"klarna",
+         "name":"Klarna Invoice",
+       "logo":"https://live.adyen.com/hpp/img/pm/klarna_small.png"
+      },
+      {
+         "brandCode":"afterpay_default",
+         "name":"AfterPay Invoice",
+       "logo":"https://live.adyen.com/hpp/img/pm/afterpay_default_small.pngg"
+      },
+      {
+         "brandCode":"boku",
+         "name":"Boku",
+       "logo":"https://live.adyen.com/hpp/img/pm/boku_small.png"
+      },
+      {
+         "brandCode":"paysafecard",
+         "name":"Paysafecard",
+       "logo":"https://live.adyen.com/hpp/img/pm/paysafecard_small.png"
+      },
+      {
+         "brandCode":"paypal",
+         "name":"PayPal",
+       "logo":"https://live.adyen.com/hpp/img/pm/paypal_small.png"
+      }
+   ]
+}
